e767cdeaf8727cf515c779cf37a8d89299580cd4
SkorikSergey <sskorik@codenvy.com>
2018-03-07 11:07:51 +0200

    changed screenshots names

--- selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/miscellaneous/NavigateToFileTest.java
+++ selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/miscellaneous/NavigateToFileTest.java
@@ -167,7 +167,7 @@ public class NavigateToFileTest {
     navigateToFile.waitSuggestedPanel();
     waitExpectedItemsInNavigateToFileDropdown(expectedItems);
 
-    String filename = NameGenerator.generate("", 8) + ".png";
+    String filename = NameGenerator.generate("NavigateToFile", 4) + ".png";
     try {
       byte[] data = seleniumWebDriver.getScreenshotAs(OutputType.BYTES);
       Path screenshot = Paths.get("./target/screenshots/", filename);
